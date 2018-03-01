822b15049899027ff1a26882429c09576e5a3a0f
Unknown <w6w54@students.keele.ac.uk>
2018-03-01 17:08:28 +0000

    test
    testing interaction

--- HotelManagment/src/hotelmanagment/Customer.java
+++ HotelManagment/src/hotelmanagment/Customer.java
@@ -10,41 +10,125 @@ package hotelmanagment;
  * @author Alex
  */
 public class Customer {
-    String Name;
-    //Address address;
-    String Phonenumber;
-    String Email;
-    String Roomnumber;
-    String occupants;
+    private int ID;
+    private String Name;
+    private String houseNumber;
+    private String roadName;
+    private String town;
+    private String county;
+    private String postCode;
+    private String phoneNumber;
+    private String email;
+    private String roomNumber;
+    private String occupants;
     
-    private Customer(){
-        this.Name = "//no name\\";
-        this.Phonenumber = "//no name\\";
-        this.Email = "//no name\\";
-        this.Roomnumber = "//no name\\";
-        this.occupants = "//no name\\";
+    public Customer(){
+        this.Name = "//none\\";
+        this.houseNumber = "//none\\";
+        this.roadName = "//none\\";
+        this.town = "//none\\";
+        this.county = "//none\\";
+        this.postCode = "//none\\";
+        this.phoneNumber = "//none\\";
+        this.email = "//none\\";
+        this.roomNumber = "//none\\";
+        this.occupants = "//none\\";
         
         
+    }
+    
+    public int getID(){
+        return this.ID;
+    }
+    
+    public void setID(int num){
+        this.ID = num;
     }
     
     public void setName(String name){
         this.Name = name;
     }
     
+    public void setHousenumber(String name){
+        this.houseNumber = name;
+    }
+    
+    public void setRoadname(String name){
+        this.roadName = name;
+    }
+    
+    public void setTown(String name){
+        this.town = name;
+    }
+    
+    public void setCounty(String name){
+        this.county = name;
+    }
+    
+    public void setPostcode(String name){
+        this.postCode = name;
+    }
+    
     public void setPhonenumber(String name){
-        this.Phonenumber = name;
+        this.phoneNumber = name;
     }
     
     public void setEmail(String name){
-        this.Email = name;
+        this.email = name;
     }
     
-    public void setRoomnuber(String name){
-        this.Roomnumber = name;
+    public void setRoomnumber(String name){
+        this.roomNumber = name;
     }
     
     public void setOccupants(String name){
         this.occupants = name;
     }
     
+    public String getName(){
+        return this.Name;
+    }
+    
+    public String getHousenumber(){
+        return this.houseNumber;
+    }
+    
+    public String getRoadname(){
+        return this.roadName;
+    }
+    
+    public String getTown(){
+        return this.town;
+    }
+    
+    public String getCounty(){
+        return this.county;
+    }
+    
+    public String getPostcode(){
+        return this.postCode;
+    }
+    
+    public String getPhonenumber(){
+        return this.phoneNumber;
+    }
+    
+    public String getEmail(){
+        return this.email;
+    }
+    
+    public String getRoomnumber(){
+        return this.roomNumber;
+    }
+    
+    public String getOccupants(){
+        return this.occupants;
+    }
+    
+    @Override
+    public String toString(){
+        String x;
+        x = "=========================\nID: " +  String.valueOf(this.ID) + " \nName: " + this.Name + "\nAddress:\n" + this.houseNumber + "\n" + this.roadName + "\n" + this.town + "\n" + this.county + "\n" + this.postCode + "\n" +"Email: "+ this.email +"\nPhone Number: "+ this.phoneNumber + "\nOccupants: " + this.occupants +"\n =========================";
+        return x;
+    }
 }
--- HotelManagment/src/hotelmanagment/HotelManagment.java
+++ HotelManagment/src/hotelmanagment/HotelManagment.java
@@ -5,21 +5,25 @@
  */
 package hotelmanagment;
 
+import java.util.ArrayList;
+import java.util.List;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class HotelManagment extends Application{
-
+List<TextField> inputs = new ArrayList();
+List<Customer> customers = new ArrayList();
 
     
     public static void main(String[] args) {
@@ -28,41 +32,106 @@ public class HotelManagment extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        //Charlie sucks knobs
         Stage window = primaryStage;
+        Scene scene;
+        Pane Pane = new Pane();
+        scene = new Scene(Pane);
+        TabPane tabs = new TabPane();
+        Pane.getChildren().add(tabs);
+        Tab p1 = new Tab("New Customer");
+        p1.setClosable(false);
+
+        p1.setContent(window1());
         
-        window.setScene(window1());
+        tabs.getTabs().add(p1);
+        
+        window.setScene(scene);
         
         window.show();     
     }
     
-    private Scene window1(){
+    private BorderPane window1(){
         //Building Window
-        Scene scene;        
-        BorderPane Pane = new BorderPane();
-        scene = new Scene(Pane);
-        VBox disp = new VBox();
-        HBox Nameline = new HBox();
-        
-        
-        //Building shit to put on window
-        Pane pane = new Pane();
-        Label Nlabel = new Label("Name: ");
-        TextField name = new TextField();
-        name.setPromptText("Input Customer Name");
-        
-        
+        BorderPane BPane = new BorderPane();
+        VBox input = new VBox();
         
         
         //putting it on the window
-        pane.getChildren().addAll(Nlabel);
-        Nameline.getChildren().addAll(pane, name);
-        disp.setMinSize(200, 100);
-        disp.getChildren().addAll(Nameline);
+        BPane.setCenter(input);
+        input.setMinSize(200, 100);
+        input.getChildren().addAll(
+                inLine("Name"), 
+                inLine("Phone Number"),
+                inLine("Email"),
+                inLine("House Number"),
+                inLine("Road Name"),
+                inLine("Town"),
+                inLine("County"),
+                inLine("Post Code"),
+                inLine("Group size")
+        );
         
-        Pane.setCenter(disp);
+        Button action = new Button("Action");
+        action.setOnAction(e -> {
+            Customer cust = new Customer();
+            cust.setID(customers.size() + 1);
+            cust.setName(get("Name"));
+            cust.setEmail(get("Email"));
+            cust.setPhonenumber(get("Phone Number"));
+            cust.setOccupants(get("Group size"));
+            cust.setHousenumber(get("House Number"));
+            cust.setRoadname(get("Road Name"));
+            cust.setTown(get("Town"));
+            cust.setCounty(get("County"));
+            cust.setPostcode(get("Post Code"));
+            customers.add(cust);
+            
+            
+            //troubleshooting loops
+            /*for(TextField i: inputs){
+            System.out.println(i.getId());
+            }
+            */
+            
+            for(int i = 0; i< customers.size(); i++){
+            System.out.println(customers.get(i));
+            }
+            
+        });
         
-        return scene;
         
+        input.getChildren().add(action);
+        
+        return BPane;
+        
+    }
+    
+    private HBox inLine(String in){
+        String input = in;
+        HBox line = new HBox();
+        Label label = new Label(in + ": ");
+        TextField Tfield = new TextField();
+        Tfield.setPromptText("Input " + in);
+        Tfield.setId(in);
+        line.getChildren().addAll(label, Tfield);
+        inputs.add(Tfield);
+        return line;
+    }
+    
+    private String get(String what){
+        
+        for(TextField data: inputs){
+            if(what.equals(data.getId())){
+                //System.out.println(what +" == "+ data.getId());
+                return data.getText();
+                
+            }
+            /*else{
+            System.out.println(what +" != "+ data.getId());
+            }*/
+        }
+        return "Not Found";
     }
     
 }
